5f9e18db00345eab30dd12577bc1d6e433515c5c
ryoppiP-P <ryoto.kikuchi@gmail.com>
2025-03-17 22:07:08 +0900

    カメラがマウスカーソルで移動するよ。
    マウスカーソルで移動できるようにしました。
    移動範囲は画面範囲内に収まるように調整しました

---
+++ Assets/honban/SubCameraScript.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class SubCameraScript : MonoBehaviour {
+    private GameObject[] subCameraObjects;
+    public bool isSubCameraActive;
+
+    void Start() {
+        // タグを使ってすべてのカメラオブジェクトを取得
+        subCameraObjects = GameObject.FindGameObjectsWithTag("SubCamera");
+
+        // 初期設定として SubCamera を無効にする
+        isSubCameraActive = false;
+        SetSubCameraObjectsActive(isSubCameraActive);
+    }
+
+    void Update() {
+        // Mキーが押されたときにカメラを切り替える
+        if (Input.GetKeyDown(KeyCode.M)) {
+            isSubCameraActive = !isSubCameraActive;
+            SetSubCameraObjectsActive(isSubCameraActive);
+        }
+    }
+
+    private void SetSubCameraObjectsActive(bool isActive) {
+        //foreach (GameObject obj in subCameraObjects) {
+        //    obj.SetActive(isActive);
+        //}
+        for (int i = 0; i < subCameraObjects.Length; i++) {
+            GameObject obj = subCameraObjects[i];
+            obj.SetActive(isActive);
+        }
+    }
+}
